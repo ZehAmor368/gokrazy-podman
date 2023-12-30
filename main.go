dd6316b45dcb95d6b574440100b9328c4b61b0c4
Mathis Poetzsch <53173296+ZehAmor368@users.noreply.github.com>
2023-12-30 14:46:57 +0100

    rename storage volume

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func grafana() error {
 		"-td",
 		"--network", "host",
 		"--name", "grafana",
-		"--volume", "/perm/grafana:/var/lib/grafana",
+		"--volume", "/var/lib/grafana:/var/lib/grafana",
 		"-p3000:3000",
 		"docker.io/grafana/grafana:latest")
 
